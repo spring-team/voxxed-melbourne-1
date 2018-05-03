06e3fd4458138b56af41604baafc17b6df5d4750
Josh Long <starbuxman@gmail.com>
2018-05-03 10:33:47 +1000

    initial commit

---
+++ src/main/java/com/example/excelsior/ExcelsiorApplication.java
@@ -0,0 +1,68 @@
+package com.example.excelsior;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
+
+@SpringBootApplication
+public class ExcelsiorApplication {
+
+		@Bean
+		ApplicationRunner runner(PersonRepository personRepository) {
+				return args -> {
+						Arrays
+							.asList("Dr. Johnson", "Dr. Syer", "Dr. De Volder", "Dr. Pollack")
+							.forEach(name -> personRepository.save(new Person(null, name)));
+
+						personRepository.findAll().forEach(System.out::println);
+				};
+
+		}
+
+		public static void main(String[] args) {
+				SpringApplication.run(ExcelsiorApplication.class, args);
+		}
+}
+
+@RestController
+class PersonRestController {
+
+		private final PersonRepository personRepository;
+
+		PersonRestController(PersonRepository personRepository) {
+				this.personRepository = personRepository;
+		}
+
+		@GetMapping("/people")
+		Collection<Person> people() {
+				return this.personRepository.findAll();
+		}
+}
+
+interface PersonRepository extends JpaRepository<Person, Long> {
+}
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class Person {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+		private String name;
+}
